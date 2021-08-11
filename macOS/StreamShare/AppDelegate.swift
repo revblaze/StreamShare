693a09b148b330a9065d4ef63d541e1615bd338c
Justin Bush <revblaze@users.noreply.github.com>
2021-08-11 11:58:02 -0400

    Create project

---
+++ macOS/StreamShare/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  StreamShare
+//
+//  Created by Justin Bush on 2020-12-30.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    // Handles Reopening of Main Window
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window in sender.windows {
+                window.makeKeyAndOrderFront(self)
+            }
+        }
+        return true
+    }
+
+}
+
---
+++ macOS/StreamShare/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StreamShare
+//
+//  Created by Justin Bush on 2020-12-30.
+//
+
+import Cocoa
+import WebKit
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
